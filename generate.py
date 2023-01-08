927c3753d69e0d0cc41218ea49044f021fad8889
Pablo Torres Anaya <pablo.ta@ugr.es>
2023-01-08 16:22:38 +0100

    add latex document content

--- generate.py
+++ generate.py
@@ -3,11 +3,19 @@ import describe
 import glob
 import re
 
+intro="""\documentclass{article}
+\usepackage[utf8]{inputenc}
+\usepackage{graphicx}
+\usepackage{markdown}
+
+\begin{document}
+"""
+
 template = r"""
 \begin{figure}[t]
 \includegraphics[width=0.9\textwidth]{%02d/map.png}
 \end{figure}
-\input{%02d/map}
+\input{%02d/map.md}
 """
 
 @click.command()
@@ -16,8 +24,9 @@ template = r"""
 def main(directory, n):
     describe.do_novel(directory, n)
     with open(directory + "/contents.tex", "w") as f:
+        f.write(intro)
         for i in range(n):
             f.write(template % (i,i))
-
+        f.write("\end{document}")
 if __name__ == '__main__':
     main()
